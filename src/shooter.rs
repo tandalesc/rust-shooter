1cffbfd2053eb9c14bc5b06aee337301932b1abd
Shishir Tandale <shishir@tandale.com>
2019-09-15 21:34:30 -0400

    allow player to level up their weapon

--- src/shooter.rs
+++ src/shooter.rs
@@ -3,7 +3,7 @@ use trees::{tr, bfs, Tree, Node};
 use trees::linked::fully::iter::{Iter, IterMut};
 use std::collections::LinkedList;
 
-use crate::state::{RESOLUTION, GRID_RESOLUTION, BULLET_SPEED, BULLET_SIZE, FRICTION};
+use crate::state::{RESOLUTION, GRID_RESOLUTION, BULLET_SIZE, FRICTION};
 
 pub type Point2 = na::Point2<f32>;
 pub type Vector2 = na::Vector2<f32>;
@@ -23,9 +23,11 @@ impl Hitbox {
     pub fn new_square(point: Point2, size: f32) -> Hitbox {
         Hitbox::new(point, Vector2::new(size, size))
     }
+    //move bounding box by a certain amount
     pub fn move_delta(&mut self, delta: Vector2) {
         self.point += delta;
     }
+    //standard bounding box collision
     pub fn collides_with(&self, other: &Hitbox) -> bool {
         self.point.x < other.point.x+other.size.x &&
         self.point.x+self.size.x > other.point.x &&
@@ -45,12 +47,15 @@ impl HitboxTree {
         }
     }
     pub fn collides_with(&self, other: &HitboxTree) -> bool {
+        //we are using LinkedList as a light-weight queue (need to add to end and pop from beginning)
         let mut self_queue: LinkedList<&Node<Hitbox>> = LinkedList::new();
         let mut other_queue: LinkedList<&Node<Hitbox>> = LinkedList::new();
+        //seed with root nodes
         let (self_root, other_root) = (self.tree.root(), other.tree.root());
         self_queue.push_back(self_root);
         other_queue.push_back(other_root);
         //preliminary collision
+        //get location on 10x10 grid and check to see if objects are in the same grid tile
         let self_grid = get_grid_square(self_root.data.point);
         let other_grid = get_grid_square(other_root.data.point);
         if (self_grid.0-other_grid.0).abs() > 1.0 ||
@@ -58,27 +63,35 @@ impl HitboxTree {
             return false;
         }
         //hierarchical collision
+        //start at root node, and attempt to find a collision between leaf nodes
         while let (Some(self_node), Some(other_node))=(self_queue.front(), other_queue.front()) {
+            //collision found! (using bounding box algorithm on current Hitbox node)
             if self_node.data.collides_with(&other_node.data) {
+                //if both are leaf nodes, then we've found the most specific collision we can
                 if self_node.is_leaf() && other_node.is_leaf() {
                     return true;
                 } else if other_node.degree()>0 {
+                    //if the other node has children, explore those next
                     other_node.iter().for_each(|node| other_queue.push_back(node));
                     other_queue.pop_front();
                 } else if self_node.degree()>0 {
+                    //otherwise, explore this node's children if it has any
                     self_node.iter().for_each(|node| self_queue.push_back(node));
                     self_queue.pop_front();
                 }
             } else {
+                //if self_node is a leaf node, then throw it away and explore the next node
                 if self_node.is_leaf() {
                     self_queue.pop_front();
                 } else {
+                    //otherwise, throw away the other node and explore what's next
                     other_queue.pop_front();
                 }
             }
         }
         false
     }
+    //helper to move all bounding boxes in this tree by a certain amount
     pub fn move_delta(&mut self, delta: Vector2) {
         self.bfs_iter_mut().for_each(|hitbox_visit| hitbox_visit.data.move_delta(delta));
     }
@@ -90,10 +103,12 @@ impl HitboxTree {
     }
 }
 
+//helper trait so we can check for collisions, positions, and sizes for any of our objects
 pub trait GameObject {
     fn get_position(&self) -> Point2;
     fn get_size(&self) -> Vector2;
     fn get_hitbox_tree(&self) -> Option<&HitboxTree>;
+    //default implementation -- get hitbox trees (if they exist) and do standard collision check
     fn collides_with(&self, other: &dyn GameObject) -> bool {
         if let (Some(sht), Some(oht)) = (self.get_hitbox_tree(), other.get_hitbox_tree()) {
             sht.collides_with(&oht)
@@ -109,6 +124,8 @@ pub struct Player {
     pub velocity: Vector2,
     pub size: f32,
     pub health: f32,
+    pub experience: f32,
+    pub weapon_level: i16,
     pub invincibility_frames: i16,
     pub bullet_spacing: i16,
     pub hitbox_tree: HitboxTree
@@ -122,13 +139,15 @@ impl Player {
             velocity: Vector2::new(0.0, 0.0),
             size: size,
             health: 100.0,
+            experience: 0.0,
+            weapon_level: 0,
             bullet_spacing: 0,
             invincibility_frames: 0,
             hitbox_tree: HitboxTree::new(
-                tr(Hitbox::new_square(pos, size))
-                    /( tr(Hitbox::new(pos+Vector2::new(size/7.0, size/3.0), Vector2::new(5.0*size/7.0, size/3.0))) )
-                    /( tr(Hitbox::new(pos+Vector2::new(size/20.0, 2.0*size/5.0), Vector2::new(18.0*size/20.0, size/5.0))) )
-                    /( tr(Hitbox::new(pos+Vector2::new(size/3.0, size/7.0), Vector2::new(size/3.0, 5.0*size/7.0))) )
+                tr(Hitbox::new_square(pos, size)) //root
+                    /( tr(Hitbox::new(pos+Vector2::new(size/7.0, size/3.0), Vector2::new(5.0*size/7.0, size/3.0))) ) //fuselage
+                    /( tr(Hitbox::new(pos+Vector2::new(size/20.0, 2.0*size/5.0), Vector2::new(18.0*size/20.0, size/5.0))) ) //wings
+                    /( tr(Hitbox::new(pos+Vector2::new(size/3.0, size/7.0), Vector2::new(size/3.0, 5.0*size/7.0))) ) //engines
             )
         }
     }
@@ -184,9 +203,9 @@ impl Enemy {
             health: 100.0,
             flash_frames: 0,
             hitbox_tree: HitboxTree::new(
-                tr(Hitbox::new_square(position, size))
-                    /( tr(Hitbox::new(position+Vector2::new(0.0, 2.0*size/5.0), Vector2::new(size, size/7.0))) )
-                    /( tr(Hitbox::new(position+Vector2::new(size/3.0, 0.0), Vector2::new(size/3.0, 7.0*size/10.0))) )
+                tr(Hitbox::new_square(position, size)) //root
+                    /( tr(Hitbox::new(position+Vector2::new(0.0, 2.0*size/5.0), Vector2::new(size, size/7.0))) ) //wings
+                    /( tr(Hitbox::new(position+Vector2::new(size/3.0, 0.0), Vector2::new(size/3.0, 7.0*size/10.0))) ) //fuselage
             )
         }
     }
@@ -250,6 +269,8 @@ impl GameObject for Bullet {
     }
 }
 
+//convert a Point2 to a discrete location on a grid
+//returns (f32, f32) as a convinience, but all components are floored
 fn get_grid_square(p: Point2) -> (f32, f32) {
     ((p.x*GRID_RESOLUTION.0/RESOLUTION.0).floor(), (p.y*GRID_RESOLUTION.1/RESOLUTION.1).floor())
 }
--- src/state.rs
+++ src/state.rs
@@ -11,13 +11,14 @@ use std::collections::HashMap;
 use crate::shooter::{Vector2, Point2, Player, Enemy, Bullet, GameObject};
 
 const BULLET_DAMAGE: f32 = 5.0;
-const BULLET_SPACING_T: i16 = 12;
 const BULLET_SPACING_X: f32 = 5.0;
 const BULLET_SPACING_Y: f32 = 0.7;
+//bullet-spacing in time axis (num frames)
+const BULLET_SPACING_T: i16 = 10;
 const BULLET_OFFSET: f32 = -3.0;
 const BULLET_SPREAD: i16 = 2;
 const BULLET_ANGLE: f32 = 0.3;
-pub const BULLET_SPEED: f32 = 5.0;
+const BULLET_SPEED: f32 = 5.0;
 pub const BULLET_SIZE: f32 = 10.0;
 
 const ENEMIES: (u8,u8) = (7, 3);
@@ -25,9 +26,11 @@ const ENEMY_SHOOT_CHANCE: usize = 50;
 
 const PLAYER_INVINCIBILITY: i16 = 60;
 const HITBOX_COLOR: (f32, f32, f32, f32) = (1.0, 0.1, 0.1, 0.4);
+
 pub const FRICTION: f32 = 0.1;
 
 pub const RESOLUTION: (f32, f32) = (640.0, 480.0);
+//sub-divides screen into 10x10 grid for the purposes of speeding up collision detection
 pub const GRID_RESOLUTION: (f32, f32) = (10.0, 10.0);
 
 const SHOW_FRAMERATE: bool = false;
@@ -37,9 +40,9 @@ pub struct State {
     player: Player,
     bullets: HashMap<Uuid, Bullet>,
     enemy_bullets: HashMap<Uuid, Bullet>,
-    bullet_ids: HashSet<Uuid>,
-    enemy_bullet_ids: HashSet<Uuid>,
     enemies: HashMap<Uuid, Enemy>,
+    bullet_ids: HashSet<Uuid>,
+    enemy_ids: HashSet<Uuid>,
     keys: HashSet<KeyCode>,
     rng: ThreadRng,
     spritebatch_player: SpriteBatch,
@@ -53,16 +56,17 @@ impl State {
             player: Player::new(),
             bullets: HashMap::new(),
             enemy_bullets: HashMap::new(),
-            bullet_ids: HashSet::new(),
-            enemy_bullet_ids: HashSet::new(),
             enemies: HashMap::new(),
-            rng: rand::thread_rng(),
+            bullet_ids: HashSet::new(),
+            enemy_ids: HashSet::new(),
             keys: HashSet::with_capacity(6),
+            rng: rand::thread_rng(),
             spritebatch_player: SpriteBatch::new(Image::new(ctx, "/player.png").unwrap()),
             spritebatch_bullet: SpriteBatch::new(Image::new(ctx, "/bullet.png").unwrap()),
             spritebatch_enemy: SpriteBatch::new(Image::new(ctx, "/enemy.png").unwrap())
         };
         //generate a grid of enemies
+        //todo: refactor into it's own method so we can generate enemies on the fly
         for x in 0..ENEMIES.0 {
             for y in 0..ENEMIES.1 {
                 state.enemies.insert(
@@ -93,8 +97,10 @@ impl State {
                     self.player.velocity += Vector2::new(0.0, 1.0);
                 }
                 KeyCode::Space => {
+                    //todo: refactor into different WeaponTypes and allow switching
                     if self.player.bullet_spacing==0 {
-                        for _bullet_num in -BULLET_SPREAD..(BULLET_SPREAD+1) {
+                        //scale number of bullets with weapon level
+                        for _bullet_num in -self.player.weapon_level..(self.player.weapon_level+1) {
                             let bullet_num = (_bullet_num*2) as f32;
                             let velocity = Vector2::new(BULLET_ANGLE*bullet_num, -BULLET_SPEED);
                             let offset = Vector2::new(BULLET_SPACING_X*bullet_num, (BULLET_SPACING_Y*bullet_num).powf(2.0)+BULLET_OFFSET);
@@ -113,8 +119,10 @@ impl State {
     }
     fn handle_bullets(&mut self, _ctx: &mut Context) -> GameResult<()> {
         //check player bullets and enemies for collisions
-        for (_, enemy) in &mut self.enemies {
-            for (bullet_id, bullet) in &self.bullets {
+        for (bullet_id, bullet) in &mut self.bullets {
+            //apply physics to player bullets
+            bullet.physics();
+            for (_, enemy) in &mut self.enemies {
                 if bullet.collides_with(enemy) {
                     //mark bullet for deletion
                     self.bullet_ids.insert(*bullet_id);
@@ -129,65 +137,88 @@ impl State {
             }
         }
         //check enemy bullets and player for collisions
-        for (bullet_id, bullet) in &self.enemy_bullets {
+        for (bullet_id, bullet) in &mut self.enemy_bullets {
+            //apply physics to enemy bullets
+            bullet.physics();
+            //only check for collisions if player is not invincible
             if self.player.invincibility_frames==0 && bullet.collides_with(&self.player) {
-                self.enemy_bullet_ids.insert(*bullet_id);
+                //mark bullet for deletion
+                self.bullet_ids.insert(*bullet_id);
+                //do damage
                 self.player.health -= 10.0;
                 self.player.invincibility_frames = PLAYER_INVINCIBILITY;
             } else if bullet.position[0]<(-bullet.size) || bullet.position[0]>RESOLUTION.0 ||
                 bullet.position[1]<(-bullet.size) || bullet.position[1]>RESOLUTION.1 {
-                self.enemy_bullet_ids.insert(*bullet_id);
+                //mark bullet for deletion
+                self.bullet_ids.insert(*bullet_id);
             }
         }
         //remove any bullets that were marked for deletion
-        for id in &self.enemy_bullet_ids { self.enemy_bullets.remove(id); }
-        for id in &self.bullet_ids { self.bullets.remove(id); }
-        self.enemy_bullet_ids.clear();
+        for id in &self.bullet_ids { self.bullets.remove(id); self.enemy_bullets.remove(id); }
         self.bullet_ids.clear();
         Ok(())
     }
-}
-
-impl EventHandler for State {
-    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        if timer::ticks(ctx) % 50 == 0 {
-            if SHOW_FRAMERATE {
-                println!("Average FPS: {}, #Bullets: {}", timer::fps(ctx), self.bullets.len()+self.enemy_bullets.len());
-            }
-        }
-
-        self.handle_keys(ctx)?;
-        self.handle_bullets(ctx)?;
-        self.player.physics();
+    fn handle_enemies(&mut self, _ctx: &mut Context) -> GameResult<()> {
         let num_enemies = self.enemies.len();
-        for (_, enemy) in &mut self.enemies {
+        for (enemy_id, enemy) in &mut self.enemies {
             enemy.physics();
+            //scale shooting chance with number of enemies
+            //more enemies = each one shoots less frequently
             if self.rng.gen_range(0, ENEMY_SHOOT_CHANCE*num_enemies)==0 {
                 let offset = Vector2::new(0.0, 20.0);
                 //bullets go towards player
                 let velocity = (self.player.position - enemy.position).normalize()*BULLET_SPEED;
                 self.enemy_bullets.insert(Uuid::new_v4(), Bullet::new(enemy, velocity, Some(offset)));
             }
+            //only check for collisions if player is not invincible
             if self.player.invincibility_frames==0 && enemy.collides_with(&self.player) {
                 self.player.health -= 20.0;
                 self.player.invincibility_frames = PLAYER_INVINCIBILITY;
             }
+            if enemy.health <= 0.0 {
+                //mark enemy for removal and add experience to player
+                self.enemy_ids.insert(*enemy_id);
+                self.player.experience += 50.0/((self.player.weapon_level+1) as f32);
+            }
         }
-        for (_, bullet) in &mut self.bullets {
-            bullet.physics();
-        }
-        for (_, bullet) in &mut self.enemy_bullets {
-            bullet.physics();
+        //remove any enemies that died
+        for id in &self.enemy_ids { self.enemies.remove(id); };
+        self.enemy_ids.clear();
+        Ok(())
+    }
+}
+
+impl EventHandler for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        //only print out periodically
+        if timer::ticks(ctx) % 50 == 0 {
+            if SHOW_FRAMERATE {
+                println!("Average FPS: {}, #Bullets: {}", timer::fps(ctx), self.bullets.len()+self.enemy_bullets.len());
+            }
         }
 
-        //remove any enemies that died
-        self.enemies.retain(|_, enemy| enemy.health > 0.0);
-        //win state
+        //better handle key presses and releases
+        self.handle_keys(ctx)?;
+        //apply game logic to bullets (and enemies/players who are hit)
+        self.handle_bullets(ctx)?;
+        //apply game logic to enemies
+        self.handle_enemies(ctx)?;
+
+        //handle player movement
+        self.player.physics();
+        //level up
+        if self.player.experience >= 100.0 {
+            println!("\nlevel up!\n");
+            self.player.experience = 0.0;
+            self.player.weapon_level += 1;
+        }
+
+        //win states
         if self.enemies.len() == 0 {
             println!("\nyou win!\n");
             event::quit(ctx);
         }
-        //lose state
+        //lose states
         if self.player.health <= 0.0 {
             println!("\nyou lose!\n");
             event::quit(ctx);
@@ -197,14 +228,16 @@ impl EventHandler for State {
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, graphics::BLACK);
 
-        //render bullets
+        //render player bullets
         for (_, bullet) in &mut self.bullets {
             self.spritebatch_bullet.add(
+                //rotate bullets in the direction of movement
                 DrawParam::default()
                     .dest(bullet.position + Vector2::new(bullet.size/2.0, bullet.size/2.0))
                     .offset(Point2::new(0.5, 0.5))
                     .rotation(bullet.angle)
             );
+            //draw hitboxes for debugging
             if SHOW_HITBOXES {
                 for hitbox_visit in bullet.hitbox_tree.bfs_iter() {
                     let hitbox = hitbox_visit.data;
@@ -214,13 +247,16 @@ impl EventHandler for State {
                 }
             }
         }
+        //render enemy bullets
         for (_, bullet) in &mut self.enemy_bullets {
             self.spritebatch_bullet.add(
+                //rotate bullets in the direction of movement
                 DrawParam::default()
                     .dest(bullet.position + Vector2::new(bullet.size/2.0, bullet.size/2.0))
                     .offset(Point2::new(0.5, 0.5))
                     .rotation(bullet.angle)
             );
+            //draw hitboxes for debugging
             if SHOW_HITBOXES {
                 for hitbox_visit in bullet.hitbox_tree.bfs_iter() {
                     let hitbox = hitbox_visit.data;
@@ -235,9 +271,11 @@ impl EventHandler for State {
         for (_, enemy) in &mut self.enemies {
             let mut enemy_draw_param = DrawParam::default().dest(enemy.position);
             if enemy.flash_frames>0 {
+                //flash enemies when hit
                 enemy_draw_param = enemy_draw_param.color(Color::new(3.0, 0.8, 0.8, 1.0));
             }
             self.spritebatch_enemy.add(enemy_draw_param);
+            //draw hitboxes for debugging
             if SHOW_HITBOXES {
                 for hitbox_visit in enemy.hitbox_tree.bfs_iter() {
                     let hitbox = hitbox_visit.data;
@@ -250,9 +288,11 @@ impl EventHandler for State {
         //render player
         let mut player_draw_param = DrawParam::default().dest(self.player.position);
         if self.player.invincibility_frames>0 && self.player.invincibility_frames/(PLAYER_INVINCIBILITY/10)%2==0 {
+            //flash player when invincible
             player_draw_param = player_draw_param.color(Color::new(1.0, 1.0, 1.0, 0.1));
         }
         self.spritebatch_player.add(player_draw_param);
+        //draw hitboxes for debugging
         if SHOW_HITBOXES {
             for hitbox_visit in self.player.hitbox_tree.bfs_iter() {
                 let hitbox = hitbox_visit.data;
@@ -262,6 +302,7 @@ impl EventHandler for State {
             }
         }
 
+        //draw sprites, clear spritebatches
         graphics::draw(ctx, &self.spritebatch_bullet, DrawParam::default())?;
         graphics::draw(ctx, &self.spritebatch_enemy, DrawParam::default())?;
         graphics::draw(ctx, &self.spritebatch_player, DrawParam::default())?;
@@ -270,10 +311,24 @@ impl EventHandler for State {
         self.spritebatch_player.clear();
 
         //player hud
-        let hud_rect = Rect::new(10.0, 23.0*RESOLUTION.1/24.0-10.0, self.player.health/100.0*3.0*RESOLUTION.0/12.0, RESOLUTION.1/24.0);
-        let hud_mesh = Mesh::new_rectangle(ctx, DrawMode::fill(), hud_rect, Color::new(1.0, 0.0, 0.0, 0.4))?;
-        graphics::draw(ctx, &hud_mesh, DrawParam::default())?;
+        //health
+        //divide resolution by 12.0 or 24.0 and use as units with 10px as padding on edges
+        let hud_health_outline_rect = Rect::new(10.0, 23.0*RESOLUTION.1/24.0-10.0, 3.0*RESOLUTION.0/12.0, RESOLUTION.1/24.0);
+        let hud_health_rect = Rect::new(10.0, 23.0*RESOLUTION.1/24.0-10.0, self.player.health/100.0*3.0*RESOLUTION.0/12.0, RESOLUTION.1/24.0);
+        let hud_health_outline = Mesh::new_rectangle(ctx, DrawMode::stroke(1.2), hud_health_outline_rect, Color::new(1.0, 0.0, 0.0, 0.8))?;
+        let hud_health_filled = Mesh::new_rectangle(ctx, DrawMode::fill(), hud_health_rect, Color::new(1.0, 0.0, 0.0, 0.3))?;
+        graphics::draw(ctx, &hud_health_outline, DrawParam::default())?;
+        graphics::draw(ctx, &hud_health_filled, DrawParam::default())?;
+        //experience
+        //divide resolution by 12.0 or 24.0 and use as units with 10px as padding on edges
+        let hud_exp_outline_rect = Rect::new(9.0*RESOLUTION.0/12.0-10.0, 23.0*RESOLUTION.1/24.0-10.0, 3.0*RESOLUTION.0/12.0, RESOLUTION.1/24.0);
+        let hud_exp_rect = Rect::new(9.0*RESOLUTION.0/12.0-10.0, 23.0*RESOLUTION.1/24.0-10.0, self.player.experience/100.0*3.0*RESOLUTION.0/12.0, RESOLUTION.1/24.0);
+        let hud_exp_outline = Mesh::new_rectangle(ctx, DrawMode::stroke(1.2), hud_exp_outline_rect, Color::new(0.0, 1.0, 0.0, 0.8))?;
+        let hud_exp_filled = Mesh::new_rectangle(ctx, DrawMode::fill(), hud_exp_rect, Color::new(0.0, 1.0, 0.0, 0.3))?;
+        graphics::draw(ctx, &hud_exp_outline, DrawParam::default())?;
+        graphics::draw(ctx, &hud_exp_filled, DrawParam::default())?;
 
+        //end
         graphics::present(ctx)?;
         Ok(())
     }
